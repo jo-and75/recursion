7e2a2ab7d8af26f3b9026a20a5725d44c836f7cd
jo-and75 <jaotika@gmail.com>
2024-08-28 12:56:35 +0300

    Handle merging condition for elements equal to each other

--- lib/mergesort.rb
+++ lib/mergesort.rb
@@ -9,18 +9,18 @@ def merge_sort(arr,result = [])
   sorted_left_arr = merge_sort(left_arr) 
   sorted_right_arr = merge_sort(right_arr)   
   
-  while [sorted_left_arr,sorted_right_arr].none?(&:empty?)
-    if sorted_left_arr[0] < sorted_right_arr[0] 
+  while [sorted_left_arr,sorted_right_arr].none?(&:empty?) 
+    if sorted_left_arr[0] <= sorted_right_arr[0] 
       result << sorted_left_arr.shift 
     elsif sorted_right_arr[0] < sorted_left_arr[0] 
       result << sorted_right_arr.shift 
     end 
   end
-  sorted = result + sorted_left_arr + sorted_right_arr
-  p sorted
+  p sorted = result + sorted_left_arr + sorted_right_arr
 end
 
 
 a = [2,4,7,5,3,8,6,1] 
-
-merge_sort(a)
+b = [3, 2, 1, 13, 8, 5, 0, 1] 
+c = [105, 79, 100, 110]
+merge_sort(b)
